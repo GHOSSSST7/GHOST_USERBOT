017c6096ef2588e775295628fbf219f1eb06dcc5
ITALIA <115196118+sa3ed266it@users.noreply.github.com>
2022-10-08 09:21:50 +0200

    Update session.py

--- sbb_b/core/session.py
+++ sbb_b/core/session.py
@@ -31,7 +31,7 @@ except Exception as e:
     sys.exit()
 
 
-sbb_b.tgbot = tgbot = JmthonClient(
+sbb_b.tgbot = tgbot = ItaliaClient(
     session="ItaliaTgbot",
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
