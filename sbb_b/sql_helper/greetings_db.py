bbe2026d3544c7feffb76f5f14608d4e76442af7
ITALIA <115196118+sa3ed266it@users.noreply.github.com>
2022-10-10 17:27:29 +0200

    Update greetings_db.py

--- sbb_b/sql_helper/greetings_db.py
+++ sbb_b/sql_helper/greetings_db.py
@@ -1,14 +1,14 @@
-from .. import udB
+from .. import sbb_b
 
 
 def get_stuff(key=None):
-    return udB.get_key(key) or {}
+    return sbb_b.get_key(key) or {}
 
 
 def add_welcome(chat, msg, media, button):
     ok = get_stuff("WELCOME")
     ok.update({chat: {"welcome": msg, "media": media, "button": button}})
-    return udB.set_key("WELCOME", ok)
+    return sbb_b.set_key("WELCOME", ok)
 
 
 def get_welcome(chat):
@@ -20,13 +20,13 @@ def delete_welcome(chat):
     ok = get_stuff("WELCOME")
     if ok.get(chat):
         ok.pop(chat)
-        return udB.set_key("WELCOME", ok)
+        return sbb_b.set_key("WELCOME", ok)
 
 
 def add_goodbye(chat, msg, media, button):
     ok = get_stuff("GOODBYE")
     ok.update({chat: {"goodbye": msg, "media": media, "button": button}})
-    return udB.set_key("GOODBYE", ok)
+    return sbb_b.set_key("GOODBYE", ok)
 
 
 def get_goodbye(chat):
@@ -38,20 +38,20 @@ def delete_goodbye(chat):
     ok = get_stuff("GOODBYE")
     if ok.get(chat):
         ok.pop(chat)
-        return udB.set_key("GOODBYE", ok)
+        return sbb_b.set_key("GOODBYE", ok)
 
 
 def add_thanks(chat):
     x = get_stuff("THANK_MEMBERS")
     x.update({chat: True})
-    return udB.set_key("THANK_MEMBERS", x)
+    return sbb_b.set_key("THANK_MEMBERS", x)
 
 
 def remove_thanks(chat):
     x = get_stuff("THANK_MEMBERS")
     if x.get(chat):
         x.pop(chat)
-        return udB.set_key("THANK_MEMBERS", x)
+        return sbb_b.set_key("THANK_MEMBERS", x)
 
 
 def must_thank(chat):
