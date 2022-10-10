fa043d6d331f18f44a0884f17391307e6a4338ca
ITALIA <115196118+sa3ed266it@users.noreply.github.com>
2022-10-10 08:05:45 +0200

    Update __main__.py

--- sbb_b/__main__.py
+++ sbb_b/__main__.py
@@ -63,6 +63,13 @@ async def startup_process():
     return
 
 async def externalrepo():
+    if Config.EXTERNAL_REPO:
+
+        await install_externalrepo(
+
+            Config.EXTERNAL_REPO, Config.EXTERNAL_REPOBRANCH, "xtraplugins"
+
+        )
     if Config.VCMODE:
         await install_externalrepo(
             "https://github.com/sa3ed266it/CatVCPlayer", "main", "catvc"
