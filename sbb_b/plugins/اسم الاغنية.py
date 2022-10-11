9615b5a7e101955d5cb806c5f2b077b57daf3d30
ITALIA <115196118+sa3ed266it@users.noreply.github.com>
2022-10-11 22:36:05 +0200

    Update and rename اضافات2.py to اسم الاغنية.py

--- sbb_b/plugins/اسم الاغنية.py
+++ sbb_b/plugins/اسم الاغنية.py
@@ -7,23 +7,6 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from sbb_b import sbb_b
 
-@sbb_b.on(admin_cmd(pattern="حالتي ?(.*)"))
-async def _(event):
-    await event.edit("يتم التاكد من حالتك اذا كنت محظور او لا .")
-    async with bot.conversation("@SpamBot") as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=178220800)
-            )
-            await conv.send_message("/start")
-            response = await response
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.edit("اولا الغي حظر @SpamBot وحاول مجددا .")
-            return
-        await event.edit(f"- {response.message.message}")
-
-
 @sbb_b.on(admin_cmd(pattern="الاغنية ?(.*)"))
 async def _(event):
     "To reverse search music by bot."
