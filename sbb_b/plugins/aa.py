c0f281a2ff2f2cb27dc03c8900726fa8c3bb6edb
ITALIA <115196118+sa3ed266it@users.noreply.github.com>
2022-10-12 21:29:01 +0200

    Update aa.py

--- sbb_b/plugins/aa.py
+++ sbb_b/plugins/aa.py
@@ -7,7 +7,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 
-from .. import CMD_HELP, LOGS, TEMP_DOWNLOAD_DIRECTORY
+from .. import CMD_HELP, LOGS
 from ..utils import admin_cmd, edit_or_reply
 
 
@@ -103,123 +103,6 @@ async def get_full_user(event):
         except Exception as e:
             return None, e
     return None, "No input is found"
-
-
-@borg.on(admin_cmd(pattern="whois(?: |$)(.*)"))
-async def who(event):
-    cat = await edit_or_reply(
-        event, "`Sit tight while I steal some data from This guuyyy...`"
-    )
-    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
-    replied_user = await get_user(event)
-    try:
-        photo, caption = await fetch_info(replied_user, event)
-    except AttributeError:
-        await edit_or_reply(event, "`Could not fetch info of that user.`")
-        return
-    message_id_to_reply = event.message.reply_to_msg_id
-    if not message_id_to_reply:
-        message_id_to_reply = None
-    try:
-        await borg.send_file(
-            event.chat_id,
-            photo,
-            caption=caption,
-            link_preview=False,
-            force_document=False,
-            reply_to=message_id_to_reply,
-            parse_mode="html",
-        )
-        if not photo.startswith("http"):
-            os.remove(photo)
-        await cat.delete()
-    except TypeError:
-        await cat.edit(caption, parse_mode="html")
-
-
-async def get_user(event):
-    """ Get the user from argument or replied message. """
-    if event.reply_to_msg_id and not event.pattern_match.group(1):
-        previous_message = await event.get_reply_message()
-        replied_user = await event.client(GetFullUserRequest(previous_message.sender_id))
-    else:
-        user = event.pattern_match.group(1)
-        if user.isnumeric():
-            user = int(user)
-        if not user:
-            self_user = await event.client.get_me()
-            user = self_user.id
-        if event.message.entities:
-            probable_user_mention_entity = event.message.entities[0]
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user
-        try:
-            user_object = await event.client.get_entity(user)
-            replied_user = await event.client(GetFullUserRequest(user_object.id))
-        except (TypeError, ValueError) as err:
-            await event.edit(str(err))
-            return None
-    return replied_user
-
-
-async def fetch_info(replied_user, event):
-    """ Get details from the User object. """
-    replied_user_profile_photos = await event.client(
-        GetUserPhotosRequest(
-            user_id=replied_user.user.id, offset=42, max_id=0, limit=80
-        )
-    )
-    replied_user_profile_photos_count = "User haven't set profile pic"
-    try:
-        replied_user_profile_photos_count = replied_user_profile_photos.count
-    except AttributeError:
-        pass
-    user_id = replied_user.user.id
-    first_name = replied_user.user.first_name
-    last_name = replied_user.user.last_name
-    try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
-    except:
-        dc_id = "Couldn't fetch DC ID!"
-    common_chat = replied_user.common_chats_count
-    username = replied_user.user.username
-    user_bio = replied_user.about
-    is_bot = replied_user.user.bot
-    restricted = replied_user.user.restricted
-    verified = replied_user.user.verified
-    photo = await event.client.download_profile_photo(
-        user_id, TEMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg", download_big=True
-    )
-    first_name = (
-        first_name.replace("\u2060", "")
-        if first_name
-        else ("Fɪʀsᴛ Nᴀᴍᴇ Nᴏᴛ Fᴏᴜɴᴅ")
-    )
-    last_name = (
-        last_name.replace("\u2060", "") if last_name else ("Lᴀsᴛ Nᴀᴍᴇ Nᴏᴛ Fᴏᴜɴᴅ")
-    )
-    username = "@{}".format(username) if username else ("Nᴏ Usᴇʀɴᴀᴍᴇ Fᴏᴜɴᴅ")
-    user_bio = "Nᴏ Aʙᴏᴜᴛ/Bɪᴏ Fᴏᴜɴᴅ" if not user_bio else user_bio
-    caption = "<b>Exᴛʀᴀᴄᴛᴇᴅ Usᴇʀ Iɴғᴏ Bʏ UʟᴛʀᴀX</b>\n\n"
-    caption += f"<b>┏━━━━━━━━━━━━━━━━━━━━━</b>\n"
-    caption += f"<b>┣ Fɪʀsᴛ Nᴀᴍᴇ</b>: <code>{first_name}</code>\n"
-    caption += f"<b>┣ Sᴇᴄᴏɴᴅ Nᴀᴍᴇ</b>: <code>{last_name}</code>\n"
-    caption += f"<b>┣ Usᴇʀɴᴀᴍᴇ</b>: <i>{username}</i>\n"
-    caption += f"<b>┣ Usᴇʀ Iᴅ</b>: <code>{user_id}</code>\n"
-    caption += f"<b>┣ Dᴄ Iᴅ</b>: <code>{dc_id}</code>\n"
-    caption += f"<b>┣ Nᴏ Oғ PғP</b>: <code>{replied_user_profile_photos_count}</code>\n"
-    caption += f"<b>┣ Bᴏᴛ</b>: <code>{is_bot}</code>\n"
-    caption += f"<b>┣ Rᴇsᴛʀɪᴄᴛᴇᴅ</b>: <code>{restricted}</code>\n"
-    caption += f"<b>┣ Vᴇʀɪғɪᴇᴅ</b>: <code>{verified}</code>\n"
-    caption += f"<b>┣ Bɪᴏ</b>: <code>{user_bio}</code>\n"
-    caption += f"<b>┣ Gʀᴏᴜᴘs Iɴ Cᴏᴍᴍᴏɴ</b>: <code>{common_chat}</code>\n"
-    caption += f"<b>┣ Lɪɴᴋ Tᴏ Pʀᴏғɪʟᴇ</b>: <i><a href='tg://user?id={user_id}'>Perma Link 🚪</a></i>\n"
-    caption += f"<b>┗━━━━━━━━━━━━━━━━━━━━━</b>"
-    return photo, caption
-
 
 @borg.on(admin_cmd(pattern="link(?: |$)(.*)"))
 async def permalink(mention):
