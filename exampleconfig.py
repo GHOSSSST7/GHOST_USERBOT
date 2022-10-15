dcad04354f81c5ea690799e3a9e30a397a053184
ITALIA <115196118+sa3ed266it@users.noreply.github.com>
2022-10-15 16:20:50 +0200

    Add files via upload

---
+++ exampleconfig.py
@@ -0,0 +1,27 @@
+from sample_config import Config
+
+class Development(Config):
+
+    APP_ID = 
+    
+    API_HASH = ""
+
+    ALIVE_NAME = ""
+
+    DB_URI = ""
+
+    STRING_SESSION = ""
+
+    TG_BOT_TOKEN = ""
+    
+    TG_BOT_USERNAME = ""
+
+    PRIVATE_GROUP_BOT_API_ID = 
+    
+    PM_LOGGER_GROUP_ID = 
+    
+    COMMAND_HAND_LER = "."
+
+    SUDO_COMMAND_HAND_LER = "."
+    
+    VCMODE = "True"
